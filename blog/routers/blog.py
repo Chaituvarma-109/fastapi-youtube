b02f872f2b4892e07c05c603d7980102f2a50827
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2023-01-15 02:45:32 +0530

    finished authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,6 +3,7 @@ from .. import schemas
 from ..database import get_db
 from sqlalchemy.orm import Session
 from ..repository import blog
+from ..oauth2 import get_current_user
 
 
 router = APIRouter(
@@ -12,25 +13,25 @@ router = APIRouter(
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session=Depends(get_db)):
+def create(request: schemas.Blog, db: Session=Depends(get_db), current_user: schemas.Users = Depends(get_current_user)):
     return blog.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session=Depends(get_db)):
+def destroy(id: int, db: Session=Depends(get_db), current_user: schemas.Users = Depends(get_current_user)):
     return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog, db: Session=Depends(get_db)):
+def update(id: int, request: schemas.Blog, db: Session=Depends(get_db), current_user: schemas.Users = Depends(get_current_user)):
     return blog.update(id, request, db)
 
 
 @router.get('/', response_model=list[schemas.ShowBlog])
-def all_blog(db: Session=Depends(get_db)):
+def all_blog(db: Session=Depends(get_db), current_user: schemas.Users = Depends(get_current_user)):
     return blog.get_all(db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
-def show(id: int, db: Session = Depends(get_db)):
+def show(id: int, db: Session = Depends(get_db), current_user: schemas.Users = Depends(get_current_user)):
     return blog.show(id, db)
